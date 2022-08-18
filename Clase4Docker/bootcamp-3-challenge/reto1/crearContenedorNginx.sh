479d631de813cf669aad2c8a1fdfb468433e68fd
lauramosdim <lauramosdim7@gmail.com>
2022-08-18 14:59:16 -0500

    add reto 1 paso2 image

--- Clase4Docker/bootcamp-3-challenge/reto1/crearContenedorNginx.sh
+++ Clase4Docker/bootcamp-3-challenge/reto1/crearContenedorNginx.sh
@@ -2,11 +2,17 @@
 
 # creación del contenedor y podamos comprobar que el contenedor está funcionando.
 docker run -d --name servidor_web -p 8181:80 nginx
+echo "lo creè y este es el listado"
 docker ps
+
 #  donde se vean las imágenes que tienes en tu registro local.
+echo "estàs son las imàgenes"
 docker images
 # se elimina el contenedor. 
+echo "lo parè"
 docker stop servidor_web
-docker ps
+echo "se ve que no està corriendo"
+docker ps -a
+echo "lo eliminè y se ve que no està en la lista"
 docker rm servidor_web
 docker ps -a
